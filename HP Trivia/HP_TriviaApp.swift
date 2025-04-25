ddfe7f825f5864742aa8403c0fbdeedbf3d276d7
Elchin Ismailov <ismailowwork@gmail.com>
2025-04-25 23:43:29 +0400

    chore: initial commit with project setup and assets

---
+++ HP Trivia/HP_TriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HP_TriviaApp.swift
+//  HP Trivia
+//
+//  Created by Elchin Ismailov on 25.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct HP_TriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
